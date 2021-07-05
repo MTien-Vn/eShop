f79eb2967c2ad23c178a844f65d6c211ade877c9
MTien-Vn <manhtien2022000@gmail.com>
2021-07-05 20:51:58 +0700

    add project eShop.api

---
+++ eShop.Api/Controllers/ItemModelController.cs
@@ -0,0 +1,26 @@
+﻿using eShop.Business.Entity;
+using eShop.Business.Interface.IService;
+using eShop.Business.Model;
+using eShop.Business.ServiceRes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MISA_AMIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemModelController : BaseEntityController<Item>
+    {
+        readonly IItemModelService itemService;
+        public ItemModelController(IItemModelService _itemService) : base(_itemService)
+        {
+            this.itemService = _itemService;
+        }
+
+        [HttpPost("postItemModel")]
+        public async Task<ServiceResponse> PostItemModel(ItemModel itemModel)
+        {
+            return await itemService.SaveItemModel(itemModel);
+        }
+    }
+}
---
+++ eShop.Api/Startup.cs
@@ -0,0 +1,86 @@
+using eShop.Business.Interface.IDBConnector;
+using eShop.Business.Interface.IRepository;
+using eShop.Business.Interface.IRepository.IVendorRepository;
+using eShop.Business.Interface.IService;
+using eShop.Business.Interface.IService.IVendorService;
+using eShop.Business.ServiceImp;
+using eShop.Business.ServiceImp.VendorServiceImp;
+using eShop.Repository.DBConnectorImp;
+using eShop.Repository.RepositoryImp;
+using eShop.Repository.RepositoryImp.VendorRepositoryImp;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Misa.BL.ServiceImp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add scope dbconnector
+            services.AddScoped<IDBConnector, DBConnectorImp>();
+
+            // add scope repository
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepositoryImp<>));
+            services.AddScoped<IVendorRepository, VendorRepositoryImp>();
+            services.AddScoped<IItemModelRepository, ItemModelRepositoryImp>();
+            services.AddScoped<IStatisticTotalAmountByMonthRepository, StatisticTotalAmountByMonthRepositoryImp>();
+
+            // add scope service
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseServiceImp<>));
+            services.AddScoped<IVendorService, VendorServiceImp>();
+            services.AddScoped<IItemModelService, ItemModelServiceImp>();
+            services.AddScoped<IStatisticTotalAmountByMonthService, StatisticTotalAmountByMonthServiceImp>();
+
+            services.AddCors();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "eShop.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "eShop.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
